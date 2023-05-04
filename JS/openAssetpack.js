5466d690e63de62c695eb1cd631a14017f0bf914
stanplayzzdevs <118762037+stanplayzzdevs@users.noreply.github.com>
2023-05-04 21:39:34 +0200

    Add files via upload

---
+++ JS/openAssetpack.js
@@ -0,0 +1,13 @@
+function toggle1() {
+    var blur = document.getElementById('blur')
+    blur.classList.toggle('active')
+    var popup = document.getElementById('popup1')
+    popup.classList.toggle('active')
+}
+
+function toggle2() {
+    var blur = document.getElementById('blur')
+    blur.classList.toggle('active')
+    var popup = document.getElementById('popup2')
+    popup.classList.toggle('active')
+}
